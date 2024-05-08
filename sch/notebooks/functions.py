1d75f52e6fd015ff4584d8b9bc5b7812b3f39013
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2024-05-08 11:17:55 -0300

    Add plot donut function

--- sch/notebooks/functions.py
+++ sch/notebooks/functions.py
@@ -98,4 +98,29 @@ def plot_wordcloud(wc_docs,max_words=50):
     ax[1].axis("off")
     ax[1].set_title('Principais palavras sem bigramas')
     plt.show()
+
+def plot_donut(labels,values,title,startangle=30):
+    fig, ax = plt.subplots(figsize=(5, 3), subplot_kw=dict(aspect="equal"))
+    
+    wedges, texts = ax.pie(values, wedgeprops=dict(width=0.5), startangle=startangle)
+    
+    bbox_props = dict(boxstyle="round,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
+              bbox=bbox_props, zorder=0, va="center")
+    
+    kw = dict(arrowprops=dict(arrowstyle="-"),bbox=bbox_props,
+              zorder=0, va="center")
+    
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(labels[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
+    
+    ax.set_title(title)
+    plt.show()
     
